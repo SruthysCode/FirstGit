79a42bbd4ed7356084ddd5bbf1f334a5cdfa19ee
Sruthy <sruthyrobin@gmail.com>
2023-03-31 11:41:26 +0530

    print even numbers

--- hello.c
+++ hello.c
@@ -7,4 +7,14 @@ void main()
     printf("Enter a value \n");
     scanf("%d",&n);
 
+    printf("Even numbers are : \n");
+    for(i=0;i<n;++i)
+    {
+        if(i%2==0)
+        {
+            printf("%d \n",i);
+        }
+        
+    }
+
 }
